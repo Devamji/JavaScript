e07ec49007e29baf29d4cda2749b44b518afbb1f
Devamji <115780345+Devamji@users.noreply.github.com>
2025-04-11 22:35:34 +0530

    Update animation3.js

--- image_animation/animation3.js
+++ image_animation/animation3.js
@@ -24,15 +24,15 @@ gsap.to("#page3 h1",{
     }
 })
 
-// gsap.to("#page1 h1",{
-//     transform: "translateX(-120%)",
-//     scrollTrigger:{
-//         trigger: "#page1",
-//         scroller:"body",
-//         markeres : true,
-//         start:"top 0",
-//         end:"top -100%",
-//         scrub:5,
-//         pin:true
-//     }
-// })
+ gsap.to("#page1 h1",{
+     transform: "translateX(-120%)",
+     scrollTrigger:{
+         trigger: "#page1",
+         scroller:"body",
+         markeres : true,
+         start:"top 0",
+         end:"top -100%",
+         scrub:5,
+         pin:true
+     }
+ })
